1c17f64ee880322a2a88c11673ed6b66ab96af7d
Mitchell Lemieux <ddwk21@gmail.com>
2023-02-21 12:10:05 -0600

    first commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express')
+
+const typedefs = gql
+`
+    type Book {
+        bookId: String!
+        authors: [string]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        username: String!
+        bookCount: Int
+        savedBooks: [book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query  {
+        thisUser: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        saveBook (input: BookInfo!): User
+        removeBook(bookId: String!): User
+    }
+
+    input BookInfo{
+        bookId: String!
+        description: String!
+        authors: [String]
+        image: String
+        title: String
+        link: String
+    }
+
+`
+
+module.exports = typeDefs
