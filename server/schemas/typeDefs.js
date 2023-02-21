d7d5211d3f3defcdbd83d41168123afefff0eb6e
Mitchell Lemieux <ddwk21@gmail.com>
2023-02-21 12:28:55 -0600

    Resolvers added somewhat, tweaked typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,11 +25,12 @@ const typedefs = gql
     }
 
     type Query  {
-        thisUser: User
+        me: User
     }
 
     type Mutation {
         login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password:String!): Auth
         saveBook (input: BookInfo!): User
         removeBook(bookId: String!): User
     }
