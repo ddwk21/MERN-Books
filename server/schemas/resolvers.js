425168d264192906d34aa30ebbe68f27a3dbd717
Mitchell Lemieux <ddwk21@gmail.com>
2023-02-21 13:19:59 -0600

    Finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,6 +12,52 @@ const resolvers = {
         }
     },
     Mutation: {
-        login: async(parent, {})
-    }
+        login: async(parent, {email,password} ,context,info) =>{
+            const user = await User.findOne({email})
+
+            if(user == undefined){
+                throw new AuthenticationError(`Couldn't find User`)
+            }
+
+            const pwYes = await user.isCorrectPassword(password)
+
+            if(!pwYes){
+                throw new AuthenticationError(`Bad Password`)
+            }
+
+            const token = signToken(user)
+
+            return {user, token}
+        },
+
+        addUser: async (parent,args,context,info) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+
+            return{user, token}
+        },
+
+        saveBook: async (parent, {input}, context, info) => {
+            if(context.user) {
+                const savingUser = await User.findByIdAndUpdate(
+                    {id: context.user._id},
+                    {$addToSet: {savedBooks:input}},
+                    {new:true}
+                )
+                return savingUser
+            }
+        },
+        removeBook: async(parent,{bookId},context,info) => {
+            if(context.user) {
+                const removingUser = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks:bookId}},
+                    {new:true} 
+                )
+                return removingUser
+            }
+        }
+    },
 }
+
+module.exports = resolvers
