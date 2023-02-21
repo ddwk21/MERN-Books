d042cfc906d0ba2e2447dc75d64ab3f3553ac618
Mitchell Lemieux <ddwk21@gmail.com>
2023-02-21 12:17:29 -0600

    Got apollo into server I think?

--- server/server.js
+++ server/server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const {resolvers,typeDefs} = require('./schemas')
 
+const {ApolloServer} = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +25,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+const apolloStart = async (typeDefs, resolvers) => {
+  await apolloServer.start();
+  apolloServer.applyMiddleware({app});
+  console.log(`Apollo playground at http://localhost:${PORT}/gql`)
+}
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  apolloStart(typeDefs,resolvers);
+
 });
+
+
